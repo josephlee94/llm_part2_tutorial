6c76415bc9249ccb866bf59eef724bc74e453576
josephlee94 <josephlee.94@gmail.com>
2025-11-29 22:47:55 +0700

    Update dpo.py

--- dpo.py
+++ dpo.py
@@ -198,7 +198,7 @@ trainer = DPOTrainer(
     args=dpo_config,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    tokenizer=tokenizer,
+    processing_class=tokenizer,
     callbacks=[SimpleTestCallback()],
 )
 
